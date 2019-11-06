4d9c7b73a8992cead26437cf97f66fae7cf3682a
Javi Puche <javierpuchevictoria@gmail.com>
2019-11-06 14:27:06 +0100

    First commit

---
+++ components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="logo">
+        logo
+    </div>
+</template>
+
+<style lang="scss">
+  .logo {
+    appearance: none;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div>
+            <logo />
+            home
+        </div>
+    </div>
+</template>
+
+<script>
+    import Logo from '~/components/Logo.vue'
+
+    export default {
+        components: {
+            Logo
+        }
+    }
+</script>
